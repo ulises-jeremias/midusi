0101a303b767826656b6ab2f9c62d7a3147f9e68
ulises-jeremias <ulisescf.24@gmail.com>
2018-07-17 21:11:19 -0300

    Added sgemm product data

---
+++ sgemm_product/dataFilter.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+setwd('kitematic/sgemm_product')
+
+originalCsvData <- read.csv(file = "sgemm_product.csv", head = TRUE, sep = ',')
+
+runColnames <- c("Run1..ms.", "Run2..ms.", "Run3..ms.", "Run4..ms.")
+
+cat("CSV Data type: ", class(originalCsvData), "\n")
+cat("Number of Instances: ", nrow(originalCsvData), "\n")
+cat("Number of Attributes: ", length(originalCsvData), "\n")
+cat("Attributes Name: ", colnames(originalCsvData), "\n\n\n")
+
+filteredData <- subset(
+  originalCsvData,
+  MWG == NWG & (MWG == 64 | MWG == 128) &
+  (SA == 1 & SB == 1)
+)
+
+filteredData$Run <- rowMeans(filteredData[, runColnames], na.rm = TRUE)
+filteredData <- filteredData[, !(colnames(filteredData) %in% runColnames)]
+
+cat("Number of Instances: ", nrow(filteredData), "\n")
+cat("Number of Attributes: ", length(filteredData), "\n")
+cat("Attributes Name: ", colnames(filteredData), "\n")
+
+write.csv(filteredData, "filtered_sgemm_product.csv", row.names = FALSE)
+
+View(filteredData)
+
+# plots

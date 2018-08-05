6b06d19cdd6e6c391535bfb7864d8c1e68078441
ulises-jeremias <ulisescf.24@gmail.com>
2018-08-05 16:07:47 -0300

    Updated dir structure

---
+++ sgemm_product/src/data-analysis/plots.R
@@ -0,0 +1,76 @@
+#!/usr/bin/env Rscript
+
+library(ggplot2)
+
+setwd("midusi/sgemm_product/data")
+
+segmmProductData <- read.csv(file = "filtered_sgemm_product.csv", head = TRUE, sep = ",")
+
+View(segmmProductData)
+
+attach(segmmProductData)
+
+# Histogram for Run column
+
+colmax <- max(Run, na.rm = TRUE)
+colmin <- min(Run, na.rm = TRUE)
+binwidth <- (colmax - colmin)/18
+
+ggplot(data = segmmProductData, aes(x = Run)) + geom_histogram(binwidth = binwidth,
+  aes(fill = ..count..), alpha = 0.9) +
+  labs(x = "Run", y = "cantidad")
+
+ggplot(data = segmmProductData, aes(x = Run)) + geom_histogram(binwidth = binwidth,
+  aes(y = ..density.., fill = ..count..), alpha = 0.9) +
+  geom_density(col = 4) +
+  labs(x = "Run", y = "densidad")
+
+# Barplot for discrete numerical attributes
+
+ggplot(data = segmmProductData, aes(x = as.factor(MWG), fill = as.factor(MWG))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "MWG", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(NWG), fill = as.factor(NWG))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "NWG", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(KWG), fill = as.factor(KWG))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "KWG", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(MDIMC), fill = as.factor(MDIMC))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "MDIMC", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(NDIMC), fill = as.factor(NDIMC))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "NDIMC", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(KWI), fill = as.factor(KWI))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "KWI", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(VWM), fill = as.factor(VWM))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "VWM", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(VWN), fill = as.factor(VWN))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "VWN", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(STRM), fill = as.factor(STRM))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "STRM", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(STRN), fill = as.factor(STRN))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "STRN", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(SA), fill = as.factor(SA))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "SA", y = "frecuencia")
+
+ggplot(data = segmmProductData, aes(x = as.factor(SB), fill = as.factor(SB))) +
+  geom_bar() + scale_fill_hue(c = 40) +
+  labs(x = "SB", y = "frecuencia")
